d74d855557b57cb6d7bcf11e4c5f1d15d1d06754
1vanech <thisiszaxartv@gmail.com>
2025-10-02 15:35:07 +0300

    Initial commit: VansJoin plugin

---
+++ src/main/java/me/ivanech/vansjoin/VansJoin.java
@@ -0,0 +1,89 @@
+package me.ivanech.vansjoin;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class VansJoin extends JavaPlugin implements CommandExecutor {
+
+    private static VansJoin instance;
+
+    @Override
+    public void onLoad() {
+        instance = this;
+    }
+
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+        getServer().getPluginManager().registerEvents(new JoinListener(), this);
+        if (getCommand("vj") != null) {
+            getCommand("vj").setExecutor(this);
+        }
+    }
+
+    @Override
+    public void onDisable() {
+        // no-op
+    }
+
+    public static VansJoin getInstance() {
+        return instance;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        String version = getDescription().getVersion();
+        String msg = ChatColor.translateAlternateColorCodes('&', "&a[VansJoin]&r VansJoin by ivanech version " + version);
+        sender.sendMessage(msg);
+        return true;
+    }
+}
+
+package me.ivanech.vansjoin;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class VansJoin extends JavaPlugin implements CommandExecutor {
+
+    private static VansJoin instance;
+
+    @Override
+    public void onLoad() {
+        instance = this;
+    }
+
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+        getServer().getPluginManager().registerEvents(new JoinListener(), this);
+        if (getCommand("vj") != null) {
+            getCommand("vj").setExecutor(this);
+        }
+    }
+
+    @Override
+    public void onDisable() {
+        // no-op
+    }
+
+    public static VansJoin getInstance() {
+        return instance;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        String version = getDescription().getVersion();
+        String msg = ChatColor.translateAlternateColorCodes('&', "&a[VansJoin]&r VansJoin by ivanech version " + version);
+        sender.sendMessage(msg);
+        return true;
+    }
+}
+
+
